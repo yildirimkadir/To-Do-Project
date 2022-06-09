74face064086f93e42d700ca6aafa06364d42694
yildirimkadir <yildirimkad34@gmail.com>
2022-06-09 17:34:58 +0200

    "Die neuen Eigenschaften wurden hinzugefügt."

---
+++ app.js
@@ -0,0 +1,36 @@
+const add = document.querySelector(".btn");
+const input = document.querySelector("input");
+const listeDiv = document.querySelector(".liste");
+
+const ul = document.createElement("ul");
+listeDiv.appendChild(ul);
+
+add.addEventListener("click", () => {
+    if(!input.value) {
+        alert("Enter a to-do item please!")
+    } else {
+        ul.innerHTML += `<li><div><i class="far fa-check-square"></i></div>${input.value.toUpperCase()}<div><i class="fa-solid fa-trash-can"></i></div></li>`;
+        input.value ="";
+        document.querySelectorAll("li").forEach((x) => {
+        x.setAttribute("class", "todolist");
+        });
+
+    }
+
+});
+
+// const check = document.querySelector(".form-check-input");
+// const trash = document.querySelector("li i");
+
+listeDiv.addEventListener("click", (event) => {
+    if (event.target.classList.contains("fa-solid"))  {
+        event.target.parentElement.parentElement.remove();
+    }
+    else if(event.target.classList.contains("far")) {
+        event.target.style.color = "green"
+        event.target.parentElement.parentElement.style.textDecoration = "line-through";
+        
+}
+        
+
+});
